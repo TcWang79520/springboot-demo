bd38a52103a05d87314b04573cc779eab2693cee
TcWang79520 <aopp5566a@gmail.com>
2026-01-29 23:54:27 +0800

    application.properties and @VALUE

--- src/main/java/com/example/demo/component/CanonPrinter.java
+++ src/main/java/com/example/demo/component/CanonPrinter.java
@@ -1,13 +1,19 @@
 package com.example.demo.component;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CanonPrinter implements Printer {
 
+    @Value("${canon.count}")
+    private int count;
+
     @Override
     public void print(String message) {
+        count--;
         System.out.println("Canon印表機 : "+message);
+        System.out.println("剩餘使用次數: "+count);
     }
 
 }
