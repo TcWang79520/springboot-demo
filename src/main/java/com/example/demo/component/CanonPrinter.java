aa5b8bca344192c20be604f3cbdc059d754ab03c
TcWang79520 <aopp5566a@gmail.com>
2026-01-30 00:02:50 +0800

    application.properties and @VALUE (default)

--- src/main/java/com/example/demo/component/CanonPrinter.java
+++ src/main/java/com/example/demo/component/CanonPrinter.java
@@ -6,14 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class CanonPrinter implements Printer {
 
-    @Value("${canon.count}")
+    @Value("${canon.count:100}") // :100 , this means : Without Key , value will be 100
     private int count;
+
+    @Value("${my.name}")
+    private String name;
 
     @Override
     public void print(String message) {
         count--;
         System.out.println("Canon印表機 : "+message);
-        System.out.println("剩餘使用次數: "+count);
+        System.out.println(name + " 剩餘使用次數: "+count);
     }
 
 }
